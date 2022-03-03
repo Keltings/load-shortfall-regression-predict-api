78e5d464d00f10bf50e7bf3ab111b8b70a2d3b3f
Keltings <lkelida@gmail.com>
2022-03-03 18:49:00 +0300

    Finallygit add  .! a model deployed

--- model.py
+++ model.py
@@ -63,10 +63,10 @@ def _preprocess_data(data):
     df_list = list()
     # Load the dictionary as a Pandas DataFrame.
     feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
-    for i in range(0,len(feature_vector_dict)):
-        df_list.append( pd.DataFrame.from_dict([feature_vector_dict[i]]))
-    feature_vector_df1 = pd.concat(df_list ,ignore_index=True)
-    print(type(feature_vector_df))
+    # for i in range(0,len(feature_vector_dict)):
+    # df_list.append( pd.DataFrame.from_dict([feature_vector_dict[i]]))
+    # feature_vector_df1 = pd.concat(df_list ,ignore_index=True)
+    #print(type(feature_vector_df))
     #feature_vector_df = data
     # ---------------------------------------------------------------
     # NOTE: You will need to swap the lines below for your own data
@@ -77,80 +77,81 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    #predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
     #load the data
 
-    df = feature_vector_df1.copy()
-    features = ['Madrid_wind_speed', 'Bilbao_rain_1h', 'Valencia_wind_speed',
-       'Seville_humidity', 'Madrid_humidity', 'Bilbao_clouds_all',
-       'Bilbao_wind_speed', 'Seville_clouds_all', 'Bilbao_wind_deg',
-       'Barcelona_wind_speed', 'Barcelona_wind_deg', 'Madrid_clouds_all',
-       'Seville_wind_speed', 'Barcelona_rain_1h', 'Seville_rain_1h',
-       'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h',
-       'Madrid_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h',
-       'Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_pressure',
-       'Seville_weather_id', 'Valencia_pressure', 'Seville_temp_max',
-       'Madrid_pressure', 'Valencia_temp_max', 'Valencia_temp',
-       'Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity',
-       'Valencia_temp_min', 'Barcelona_temp_max', 'Madrid_temp_max',
-       'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
-       'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
-       'Madrid_temp', 'Madrid_temp_min', 'Valencia_wind_deg_level_10',
-       'Valencia_wind_deg_level_2', 'Valencia_wind_deg_level_3',
-       'Valencia_wind_deg_level_4', 'Valencia_wind_deg_level_5',
-       'Valencia_wind_deg_level_6', 'Valencia_wind_deg_level_7',
-       'Valencia_wind_deg_level_8', 'Valencia_wind_deg_level_9',
-       'Seville_pressure_sp10', 'Seville_pressure_sp11',
-       'Seville_pressure_sp12', 'Seville_pressure_sp13',
-       'Seville_pressure_sp14', 'Seville_pressure_sp15',
-       'Seville_pressure_sp16', 'Seville_pressure_sp17',
-       'Seville_pressure_sp18', 'Seville_pressure_sp19',
-       'Seville_pressure_sp2', 'Seville_pressure_sp20',
-       'Seville_pressure_sp21', 'Seville_pressure_sp22',
-       'Seville_pressure_sp23', 'Seville_pressure_sp24',
-       'Seville_pressure_sp25', 'Seville_pressure_sp3', 'Seville_pressure_sp4',
-       'Seville_pressure_sp5', 'Seville_pressure_sp6', 'Seville_pressure_sp7',
-       'Seville_pressure_sp8', 'Seville_pressure_sp9', 'Year', 'Month', 'Day',
-       'Day_of_week', 'Start_hour']
+    df = feature_vector_df.copy()
+    features = ['Madrid_wind_speed', 'Bilbao_rain_1h',
+       'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
+       'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
+       'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
+       'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h', 'Seville_rain_1h', 'Bilbao_snow_3h',
+       'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h',
+       'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
+       'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id',
+        'Seville_temp_max', 'Madrid_pressure',
+       'Valencia_temp_max', 'Valencia_temp', 'Bilbao_weather_id',
+       'Seville_temp', 'Valencia_humidity', 'Valencia_temp_min',
+       'Barcelona_temp_max', 'Madrid_temp_max', 'Barcelona_temp',
+       'Bilbao_temp_min', 'Bilbao_temp', 'Barcelona_temp_min',
+       'Bilbao_temp_max', 'Seville_temp_min', 'Madrid_temp', 'Madrid_temp_min']
+    # 'Valencia_wind_deg_level_10',
+    #    'Valencia_wind_deg_level_2', 'Valencia_wind_deg_level_3',
+    #    'Valencia_wind_deg_level_4', 'Valencia_wind_deg_level_5',
+    #    'Valencia_wind_deg_level_6', 'Valencia_wind_deg_level_7',
+    #    'Valencia_wind_deg_level_8', 'Valencia_wind_deg_level_9',
+    #    'Seville_pressure_sp10', 'Seville_pressure_sp11',
+    #    'Seville_pressure_sp12', 'Seville_pressure_sp13',
+    #    'Seville_pressure_sp14', 'Seville_pressure_sp15',
+    #    'Seville_pressure_sp16', 'Seville_pressure_sp17',
+    #    'Seville_pressure_sp18', 'Seville_pressure_sp19',
+    #    'Seville_pressure_sp2', 'Seville_pressure_sp20',
+    #    'Seville_pressure_sp21', 'Seville_pressure_sp22',
+    #    'Seville_pressure_sp23', 'Seville_pressure_sp24',
+    #    'Seville_pressure_sp25', 'Seville_pressure_sp3', 'Seville_pressure_sp4',
+    #    'Seville_pressure_sp5', 'Seville_pressure_sp6', 'Seville_pressure_sp7',
+    #    'Seville_pressure_sp8', 'Seville_pressure_sp9', 'Year', 'Month', 'Day',
+    #    'Day_of_week', 'Start_hour']
 
     for col in ['Valencia_wind_deg', 'Seville_pressure']:
         df[col] = df[col].astype('category')
 
 #have a copy of our train_data with the correct  dtype for our categorical columns
-    df_copy = df.copy()
-    df_copy['time'] = pd.to_datetime(df_copy['time'])
-    df_dummies = pd.get_dummies(df_copy, drop_first = True)
+    df_copy = df[features].copy()
+    #df_copy['time'] = pd.to_datetime(df_copy['time'])
+    #df_dummies = pd.get_dummies(df_copy, drop_first = True)
     
     # create new features
     # year
     #df_dummies['time'] = pd.to_datetime(df_dummies['time'])
-    df_dummies['Year'] = df_dummies['time'].dt.year
-    # month
-    df_dummies['Month'] = df_dummies['time'].dt.month
-    # day
-    df_dummies['Day'] = df_dummies['time'].dt.day
-    # Dayofweek
-    df_dummies['Day_of_week'] = df_dummies['time'].dt.weekday
-    # hour
-    df_dummies['Start_hour'] = df_dummies['time'].dt.hour
-    # Drop Feature
-    df_dummies = df_dummies.drop(['time','Unnamed: 0'] , axis=1)
-    print(df_dummies.shape)
+    # df_dummies['Year'] = df_dummies['time'].dt.year
+    # # month
+    # df_dummies['Month'] = df_dummies['time'].dt.month
+    # # day
+    # df_dummies['Day'] = df_dummies['time'].dt.day
+    # # Dayofweek
+    # df_dummies['Day_of_week'] = df_dummies['time'].dt.weekday
+    # # hour
+    # df_dummies['Start_hour'] = df_dummies['time'].dt.hour
+    # # Drop Feature
+    # df_dummies = df_dummies.drop(['time','Unnamed: 0'] , axis=1)
+    # print(df_dummies.shape)
+
 
     imp = IterativeImputer(max_iter=10, random_state=0)
-    imp.fit(df_dummies.values)
+    imp.fit(df_copy.values)
     IterativeImputer(random_state=0)
     
 
-    imputed = imp.transform(df_dummies.values)
+    imputed = imp.transform(df_copy.values)
 
-    predict_vector = pd.DataFrame(imputed, index=df_dummies.index, columns=df_dummies.columns)
+    predict_vector = pd.DataFrame(imputed, index=df_copy.index, columns=df_copy.columns)
     print(predict_vector.shape)
     
         
     # ------------------------------------------------------------------------
     
-    return predict_vector[features]
+    return predict_vector
 
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
